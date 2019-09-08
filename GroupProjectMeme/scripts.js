ba0f77c45f2ce705598bcf22dea744efa9a3b048
Rekkuzan5 <stapp.cameron@gmail.com>
2019-09-08 17:47:42 -0500

    Updated Princess selection function.  Status: Working.

--- GroupProjectMeme/scripts.js
+++ GroupProjectMeme/scripts.js
@@ -22,21 +22,51 @@ function myFunction() {
 	}
 }
 
-function Cinderella() {
-	document.getElementById("quote").innerHTML = "Cinderella married a man with a foot fetish.";
+// Function that changes the quote comment in the text under the princesses.  Uses mouseover and mouseout, and puts ID's into variables to compare and 
+// outputs corresponding saying based on IF loop.
+
+function selectedPrincess(x) {
+	var cinderella = document.getElementById("cinderellaID");
+	var aurora = document.getElementById("auroraID");
+	var belle = document.getElementById("belleID");
+	var jasmine = document.getElementById("jasmineID");
+	var ariel = document.getElementById("arielID");
+	var snowWhite = document.getElementById("snowWhiteID");
+
+	var cinderellaSays = "Cinderella married a man with a foot fetish.";
+	var auroraSays = "Aurora married a creep who kissed her in her sleep."
+	var belleSays = "Belle committed beasteality.";
+	var jasmineSays = "Jasmine married a dirty homeless thief.";
+	var arielSays = "Ariel risked her soul for a man she never met.";
+	var snowWhiteSays = "Snow White lived alone with 7 men.";
+
+	if (x == cinderella)
+	{
+		document.getElementById("quote").innerHTML = cinderellaSays;
+	}
+	else if (x == aurora)
+	{
+		document.getElementById("quote").innerHTML = auroraSays;
+	}
+	else if (x == belle)
+	{
+		document.getElementById("quote").innerHTML = belleSays;
+	}
+	else if	(x == jasmine)
+	{
+		document.getElementById("quote").innerHTML = jasmineSays;
+	}
+	else if	(x == ariel)
+	{
+		document.getElementById("quote").innerHTML = arielSays;
+	}
+	else if (x == snowWhite)
+	{
+		document.getElementById("quote").innerHTML = snowWhiteSays;
+	}
 }
-function Aurora() {
-	document.getElementById("quote").innerHTML = "Aurora married a creep who kissed her in her sleep.";
-}
-function Belle() {
-	document.getElementById("quote").innerHTML = "Belle committed beasteality.";
-}
-function Jasmine() {
-	document.getElementById("quote").innerHTML = "Jasmine married a dirty homeless thief.";
-}
-function Ariel() {
-	document.getElementById("quote").innerHTML = "Ariel risked her soul for a man she never met.";
-}
-function SnowWhite() {
-	document.getElementById("quote").innerHTML = "Snow White lived alone with 7 men.";
+
+function unSelectedPrincess()
+{
+	document.getElementById("quote").innerHTML = null;
 }
